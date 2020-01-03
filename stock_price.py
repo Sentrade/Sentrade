30f85585d1332a2f0be5387e842b6bb50156e34a
Davide Locatelli <dave.loca@gmail.com>
2020-01-03 16:44:19 +0000

    Basic used of yfinance added.

--- stock_price.py
+++ stock_price.py
@@ -1,9 +1,13 @@
 """
-The python package for scrapying the stock price data. yfinance can be a good package to experiment with.
+The python package for scrapying the stock price data.
 """
+import yfinance as yf
+import pandas as pd
 
-def scrap_stock():
-    pass
+def scrap_stock(index,start,end):
+    aapl = yf.download(index,start,end,group_by="ticker")
+    closeappl = aapl['Close']
+    print (closeappl)
 
 if __name__ == "__main__":
-    pass
+    scrap_stock('TSLA','2019-12-01','2019-12-30')
