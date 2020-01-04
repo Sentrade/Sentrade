05d949da767eb17106e9c3e1698713718e2109c7
Davide Locatelli <dl1019@gpu07.doc.ic.ac.uk>
2020-01-04 15:02:42 +0000

    Testing yfinance

--- stock_price.py
+++ stock_price.py
@@ -2,7 +2,6 @@
 The python package for scrapying the stock price data.
 """
 import yfinance as yf
-import pandas as pd
 
 def scrap_stock(index,start,end):
     aapl = yf.download(index,start,end,group_by="ticker")
@@ -10,4 +9,4 @@ def scrap_stock(index,start,end):
     print (closeappl)
 
 if __name__ == "__main__":
-    scrap_stock('TSLA','2019-12-01','2019-12-30')
+    scrap_stock('AAPL','2019-12-01','2019-12-30')
