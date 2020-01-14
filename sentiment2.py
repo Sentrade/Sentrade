5790b39c64bbe21052f9350c0edc1cf8d20c24ab
Davide <davide.locatelli.16@gmail.com>
2020-01-14 00:12:03 +0000

    Introduced spaCy for news filtering. Sketched integration with TextBlob

---
+++ sentiment2.py
@@ -0,0 +1,47 @@
+#author: davide
+
+"""
+pip install -U spacy
+python -m spacy download en_core_web_sm
+"""
+import json
+import spacy
+from textblob import TextBlob
+
+nlp = spacy.load("en_core_web_sm")
+
+#doc = nlp("My name is Davide.")
+#print (doc)
+#print ([t for t in doc])
+#for t in doc:
+#    print(t,t.pos_,t.dep_)
+
+index = "apple"
+#news = "Apple is looking at buying U.K. startup for $1 billion."
+
+def is_org(news,index):
+    doc = nlp(news["text"])
+    for t in doc.ents:
+        if t.lower_ == index:
+            if t.label_ == "ORG":
+                return True
+    return False
+
+def analyze_sent(file):
+    with open(file) as news_file:
+        input_data= json.load(news_file)
+
+#Integrating @Ziyou's TextBlob
+    for news in input_data:
+        if is_org(news,index):
+            blob = TextBlob(news["text"])
+            news["polarity"] = blob.sentiment.polarity
+            news["subjectivity"] = blob.sentiment.subjectivity
+
+    with open("sentiment2.json", "w") as results:
+        json.dump(input_data, results)
+
+if __name__ == "__main__":
+    analyze_sent("newsorg.json")
+
+            
