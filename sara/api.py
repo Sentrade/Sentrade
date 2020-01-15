b82d8170ebc9f1576490a4ae147b60d3f4fbdcd1
yinshaomiao <yinshaomiao@gmail.com>
2020-01-15 19:15:08 +0000

    modified api.py

--- sara/api.py
+++ sara/api.py
@@ -1,30 +1,38 @@
 import json
 from twython import Twython
 import pandas as pd
-:json stores
-: pandas
+
+companyName= 'apple'
+
 
 def getTweetInterface():
+	""
+	Function that uses credentials obtained from Twitter developer account to establish a connection to Twitter public API using the Twython Library.
+
+	:returns: Twython interface for later quueries
+	""
+
 	credentials = {}
 	credentials['CONSUMER_KEY'] = 'N1naTf3Iy8IbrwTcGWXLzOcuI'
 	credentials['CONSUMER_SECRET'] = 'zZq4yjhTuihFucyfxtJpelGO66RVv8P8tJNSgdBBHYk9oGmQLF'
-	credentials['ACCESS_TOKEN'] = '1212877716233408512-ndWwAwCCzF48k2MXdNULnCo5kWjtzo'
-	credentials['ACCESS_SECRET'] = 'jm2Zvg9J2pyq2bKRU5CaUfYBdKI6fRvvsEOk2FuADOOR7'
 
-	python_tweets = Twython(credentials['CONSUMER_KEY'], credentials['CONSUMER_SECRET'])
+	python_tweets = Twython(creds['CONSUMER_KEY'], creds['CONSUMER_SECRET'])
 	return python_tweets
-:return getTweetInterface
 
-def queryCompany(tweetInterface, company):
+
+def queryCompany(interface, companyName):
+	"""
+	Function that returns a Panda dataframe containing Tweets related to a given companyName.
+
+	:param interface: Twython interface to be queries
+	:param companyName: name of the company
+	:returns: panda dataframe containing Tweets
+	"""
 	query = {'q': company,
         'result_type': 'popular',
         'count': 10,
         'lang': 'en',
         }
-:param tweetInterface: this is a first param
-:param company: this is a second param
-:returns queryCompany
-:raises key Error:
 
 	dict_ = {'user': [], 'date': [], 'text': [], 'favorite_count': []}
 	for status in tweetInterface.search(**query)['statuses']:
@@ -32,12 +40,19 @@ def queryCompany(tweetInterface, company):
 	    dict_['date'].append(status['created_at'])
 	    dict_['text'].append(status['text'])
 	    dict_['favorite_count'].append(status['favorite_count'])
-
-	# Structure data in a pandas DataFrame for easier manipulation
+	""
+        Structure data in a pandas DataFrame for easier manipulation
+	""
+	
 	df = pd.DataFrame(dict_)
 	df.sort_values(by='favorite_count', inplace=True, ascending=False)
 	return df
 
-tweet = getTweetInterface()
-df = queryCompany(tweet, 'apple')
-print(df.head(10))
+def main():
+	""
+	Main fnction of this program.
+	""
+	interface = getTweetInterface()
+	df = queryCompanyNews(interface, companyName)
+	df.sort_values(by='favorite_cont', inplace=True, ascending=False)
+	df.head(20)
