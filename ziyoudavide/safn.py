6f880b0d11c8dd760c92bf2e818bafb29347721d
Davide <davide.locatelli.16@gmail.com>
2020-01-21 18:58:08 +0000

    basic UI coming together

--- ziyoudavide/safn.py
+++ ziyoudavide/safn.py
@@ -14,16 +14,17 @@ import datetime
 from dash.dependencies import Input, Output, State
 from plotly import tools
 
-__author__ = "Davide Locatelli"
+__author__ = "Davide Locatelli, Longzhen Li"
 __status__ = "Prototype"
 
-external_stylesheet = ["https://raw.githubusercontent.com/plotly/dash-sample-apps/master/apps/dash-web-trader/assets/style.css"]
-app = dash.Dash(__name__, meta_tags=[{"name":"viewport", "content": "width=device-width"}], external_stylesheets=external_stylesheet)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, meta_tags=[{"name":"viewport", "content": "width=device-width"}], external_stylesheets=external_stylesheets)
 server = app.server
 
 #load data
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
 
-tickers = ["AAPL","TSLA","DIS","FB"]
+tickers = ["AAPL"]
 
 #load news
 #this is just for apple, we can change according to the ticker inserted
@@ -71,91 +72,92 @@ def update_news():
     )
 
 app.layout = html.Div(
-    className="row",
     children=[
-        # Interval component for live clock
-        dcc.Interval(id="interval", interval=1 * 1000, n_intervals=0),
-        # Interval component for ask bid updates
-        dcc.Interval(id="i_bis", interval=1 * 2000, n_intervals=0),
-        # Interval component for graph updates
-        dcc.Interval(id="i_tris", interval=1 * 5000, n_intervals=0),
-        # Interval component for graph updates
-        dcc.Interval(id="i_news", interval=1 * 60000, n_intervals=0),
-        # Left Panel Div
         html.Div(
-            className="three columns div-left-panel",
+            className="top-bar",
             children=[
-                # Div for Left Panel App Info
+                html.H6(className="title-header", children="SENTRADE"),
+                html.P(
+                    """
+                    Sentiment analysis of financial news data and twitter.
+                    """
+                )
+            ], style={'textAlign': 'center'}
+        ),
+        html.Div(
+            className="dropdown",
+            children=[
+                dcc.Dropdown(
+                    options=[
+                        {'label': 'AAPL', 'value': 'AAPL'}],
+                        value=['AAPL', 'AAPL'],
+                        multi=False
+                ),
+            ], style={'width':'20%'}
+        ),
+        # Graph and news
+        html.Div(
+            className="two columns panel",
+            children=[
+                # Div for Graph
                 html.Div(
-                    className="div-info",
-                    children=[
-                        html.H6(className="title-header", children="SENTRADE"),
-                        html.P(
-                            """
-                            Describe the app here.
-                            """
-                        ),
-                    ],
+                    [
+                        dcc.Graph(id='graph-with-slider'),
+                        dcc.Slider(
+                            id='year-slider',
+                            min=df['year'].min(),
+                            max=df['year'].max(),
+                            value=df['year'].min(),
+                            marks={str(year): str(year) for year in df['year'].unique()},
+                            step=None
+                        )    
+                    ], style={'width':'100%'}
                 ),
                 # Div for News Headlines
                 html.Div(
                     className="div-news",
-                    children=[html.Div(id="news", children=update_news())],
-                ),
-            ],
+                    children=[html.Div(id="news", children=update_news())
+                    ], style={'width':'50%'}
+                ), 
+            ], style={'columnCount': 2, 'width':'100%'}
         ),
-        # Right Panel Div
-        html.Div(
-            className="nine columns div-right-panel",
-            children=[
-                # Top Bar Div - Displays 1D, 
-                #html.Div(
-                    #id="top_bar", className="row div-top-bar", children=get_top_bar()
-                #),
-                # Charts Div
-                #html.Div(
-                 #   id="charts",
-                  #  className="row",
-                   # children=[chart_div(pair) for pair in currencies],
-                #),
-                html.Div(
-                    id="bottom_panel",
-                    className="row div-bottom-panel",
-                    children=[
-                        html.Div(
-                            className="display-inlineblock",
-                            children=[
-                                dcc.Dropdown(
-                                    id="dropdown_positions",
-                                    className="bottom-dropdown",
-                                    options=[
-                                        {"label": "Open Positions", "value": "open"},
-                                        {"label": "Closed Positions","value": "closed"},
-                                    ],
-                                    value="open",
-                                    clearable=False,
-                                    style={"border": "0px solid black"},
-                                )
-                            ],
-                        ),
-                        html.Div(
-                            className="display-inlineblock float-right",
-                            children=[
-                                dcc.Dropdown(
-                                    id="closable_orders",
-                                    className="bottom-dropdown",
-                                    placeholder="Close order",
-                                )
-                            ],
-                        ),
-                        html.Div(id="orders_table", className="row table-orders"),
-                    ],
-                ),
-            ],
-        ),
-    ],
+    ] 
 )
 
+
+@app.callback(
+    Output('graph-with-slider', 'figure'),
+    [Input('year-slider', 'value')])
+def update_figure(selected_year):
+    filtered_df = df[df.year == selected_year]
+    traces = []
+    for i in filtered_df.continent.unique():
+        df_by_continent = filtered_df[filtered_df['continent'] == i]
+        traces.append(dict(
+            x=df_by_continent['gdpPercap'],
+            y=df_by_continent['lifeExp'],
+            text=df_by_continent['country'],
+            mode='markers',
+            opacity=0.7,
+            marker={
+                'size': 15,
+                'line': {'width': 0.5, 'color': 'white'}
+            },
+            name=i
+        ))
+
+    return {
+        'data': traces,
+        'layout': dict(
+            xaxis={'type': 'log', 'title': 'GDP Per Capita',
+                   'range':[2.3, 4.8]},
+            yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            legend={'x': 0, 'y': 1},
+            hovermode='closest',
+            transition = {'duration': 500},
+        )
+    }
 
 if __name__ == "__main__":
     app.run_server(debug=True)
