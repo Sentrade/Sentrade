8500f9120b34b38fd9c6dab90b0b4afe30187ea2
Davide <davide.locatelli.16@gmail.com>
2020-01-29 22:21:17 +0000

    Meet our new UI. Fake Data plotted.

--- safn.py
+++ safn.py
@@ -3,13 +3,18 @@
 
 import dash
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 import dash_html_components as html
 import chart_studio.plotly as py
 import plotly.graph_objs as go
 import requests
 import pandas as pd
 import json
-import datetime
+import datetime as dt
+import colorlover as cl
+import datetime as dt
+import flask
+import os
 
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
@@ -19,14 +24,12 @@ from data import correlation_analysis
 __author__ = "Davide Locatelli, Longzhen Li, Ziyou Zhang"
 __status__ = "Prototype"
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = dash.Dash(__name__, meta_tags=[{"name":"viewport", "content": "width=device-width"}], external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__, meta_tags=[{"name":"viewport", "content": "width=device-width"}])
 server = app.server
 
-#load data
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+colorscale = cl.scales['9']['qual']['Paired']
 
-tickers = ["AAPL"]
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/dash-stock-ticker-demo.csv')
 
 #load news
 #this is just for apple, we can change according to the ticker inserted
@@ -76,72 +79,164 @@ def update_news():
 app.layout = html.Div(
     children=[
         html.Div(
-            className="top-bar",
+            className='top-bar',
             children=[
-                html.H6(className="title-header", children="SENTRADE"),
-                html.P(
-                    """
-                    Sentiment analysis of financial news data and twitter.
-                    """
+                html.H2(
+                    'SENTRADE',
+                    style={'display': 'inline',
+                    'float': 'left',
+                    'font-size': '2.65em',
+                    'margin-left': '7px',
+                    'font-weight': '900',
+                    'font-family': 'Product Sans',
+                    'color': 'black',
+                    'margin-top': '20px',
+                    'margin-bottom': '0'
+                    }
+                ),
+                html.H4(
+                    'Financial Sentiment Analysis',
+                    style={'display': 'inline',
+                    'float': 'left',
+                    'font-size': '1.8em',
+                    'margin-left': '7px',
+                    'font-weight': '500',
+                    'font-family': 'Product Sans',
+                    'color': '#9C9C9C',
+                    'margin-top': '29px',
+                    'margin-bottom': '0'
+                    }
+                ),
+                dcc.Dropdown(
+                    id='stock-ticker-input',
+                    options=[
+                        {'label':'AMZN','value':'AMZN','disabled':True},
+                        {'label':'AAPL','value':'AAPL'},
+                        {'label':'FB','value':'FB','disabled':True},
+                        {'label':'GOOG','value':'GOOG','disabled':True},
+                        {'label':'MSFT','value':'MSFT','disabled':True},
+                        {'label':'NFLX','value':'NFLX','disabled':True},
+                        {'label':'TSLA','value':'TSLA','disabled':True},
+                        {'label':'UBER','value':'UBER','disabled':True},
+                    ],
+                    multi=False,
+                    placeholder ='Select Ticker',
+                    style={'display': 'inline',
+                    'width':'35%',
+                    'float': 'right',
+                    'font-size': '1.2em',
+                    'font-weight': '500',
+                    'font-family': 'Product Sans',
+                    'color': '#9C9C9C',
+                    'margin-top': '12px'
+                    }
                 )
-            ], style={'textAlign': 'center'}
+            ]
         ),
-
-         html.Div(
-            className="label",
-            children=[
-                html.Label('Stock ticker:',style={
-                    'textAlign': 'center'}
-                    ),
-            ], style={'width':'20%'}
-        ),
-
         html.Div(
-            className="inputbox",
-            children=[
-                dcc.Input(id='input-box', value='', type='text'),
-                html.Button('Submit', id='button'),
-            ], style={'width':'50%'}
-        ),
-
-        
-        # Graph and news
-        html.Div(
-            className="two columns panel",
-            children=[
-                # Div for Graph
-                html.Div(
-                    dcc.Graph(
-                        id='output-graph',
-                        figure={
-                            'data': [
-                             {'x': [], 'y': [], 'type': 'scatter', 'name': 'stock', 'mode': 'markers'},
-                            ],
-                            'layout': {
-                                'title': 'stock sentiment',
-                                'xaxis': {
-                                    'title':'sentiment_score'
-                                },
-                                'yaxis': {
-                                    'title':'stock_price_change'
-                                }
-                            }
-                        },
-                        style={
-                            'width':'100%'
-                            } )
-                  )  ,
-
-                # Div for News Headlines
-                html.Div(
-                    className="div-news",
-                    children=[html.Div(id="news", children=update_news())
-                    ], style={'width':'50%'}
-                ), 
-            ], style={'columnCount': 2, 'width':'100%'}
-        ),
-    ] 
+            className='graphs',
+            id='graphs',
+            style={'width':'55%','float':'center'}
+            )
+    ]
 )
+
+def bbands(price, window_size=10, num_of_std=5):
+    rolling_mean = price.rolling(window=window_size).mean()
+    rolling_std  = price.rolling(window=window_size).std()
+    upper_band = rolling_mean + (rolling_std*num_of_std)
+    lower_band = rolling_mean - (rolling_std*num_of_std)
+    return rolling_mean, upper_band, lower_band
+
+@app.callback(
+    dash.dependencies.Output('graphs','children'),
+    [dash.dependencies.Input('stock-ticker-input', 'value')])
+def update_graph(ticker):
+    graphs = []
+
+    if not ticker:
+        graphs.append(html.H3(
+            "Select a stock ticker.",
+            style={
+                'textAlign':'center',
+                'color':'#9C9C9C'
+            }
+        ))
+    else:
+        dff = df[df['Stock'] == ticker]
+        candlestick = {
+            'x': dff['Date'],
+            'open': dff['Open'],
+            'high': dff['High'],
+            'low': dff['Low'],
+            'close': dff['Close'],
+            'type': 'candlestick',
+            'name': ticker,
+            'legendgroup': ticker,
+            'increasing': {'line': {'color': colorscale[0]}},
+            'decreasing': {'line': {'color': colorscale[1]}}
+            }
+        bb_bands = bbands(dff.Close)
+        bollinger_traces = [{
+            'x': dff['Date'], 'y': y,
+            'type': 'scatter', 'mode': 'lines',
+            'line': {'width': 1, 'color': colorscale[(i*2) % len(colorscale)]},
+            'hoverinfo': 'none',
+            'legendgroup': ticker,
+            'showlegend': True if i == 0 else False,
+            'name': '{} - bollinger bands'.format(ticker)
+            } for i, y in enumerate(bb_bands)]
+        graphs.append(dcc.Graph(
+            id=ticker,
+            figure={
+                'data': [candlestick] + bollinger_traces,
+                'layout': {
+                'margin': {'b': 0, 'r': 10, 'l': 60, 't': 0},
+                'legend': {'x': 0}
+                }
+            }
+        ))
+
+    return graphs
+"""
+    # Graph and news
+    html.Div(
+        className="two columns panel",
+        children=[
+            # Div for Graph
+            html.Div(
+                dcc.Graph(
+                    id='output-graph',
+                    figure={
+                        'data': [
+                            {'x': [], 'y': [], 'type': 'scatter', 'name': 'stock', 'mode': 'markers'},
+                        ],
+                        'layout': {
+                            'title': 'stock sentiment',
+                            'xaxis': {
+                                'title':'sentiment_score'
+                                },
+                            'yaxis': {
+                                'title':'stock_price_change'
+                            }
+                        }
+                    },
+                    style={
+                        'width':'100%'
+                        }
+                )
+            ),
+            # Div for News Headlines
+            html.Div(
+                className="div-news",
+                children=[html.Div(id="news", children=update_news())], 
+                style={'width':'50%'}
+            ), 
+        ], 
+        style={'columnCount': 2, 'width':'100%'}
+    ),
+
+html.Div(className="inputbox",children=[dcc.Input(id='input-box', value='', type='text'),html.Button('Submit', id='button'),], style={'width':'50%'}),
 
 
 @app.callback(
@@ -172,6 +267,6 @@ def update_graph(no_clicked,input_value):
             }
             }           
         }
-
+"""
 if __name__ == "__main__":
     app.run_server(debug=True)
