72a91a2dfb2ff841549c9ce76c353c07519b972e
wzic <happyfrom2019@outlook.com>
2020-02-17 19:24:19 +0000

    small change on tw_extract_bz2.py

--- twitter_filtering/tw_extract_bz2.py
+++ twitter_filtering/tw_extract_bz2.py
@@ -10,5 +10,5 @@ minute = 31
 
 tw_json_bz2_filename = "./{0}/*/*.json.bz2".format(str(day))
 # .zfill(2),str(hour).zfill(2),str(minute).zfill(2))
-# subprocess.run("bunzip2 -k " + tw_json_bz2_filename, shell=True)
+subprocess.run("bunzip2 -k " + tw_json_bz2_filename, shell=True)
 subprocess.run("rm " + tw_json_bz2_filename, shell=True)
