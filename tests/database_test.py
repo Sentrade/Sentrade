4b05e169d233e6470d9257ed52983eaa12dc3c56
Ziyou Zhang <ziyou.zhang@outlook.com>
2020-02-18 04:24:47 +0000

    added test files, updated package list

---
+++ tests/database_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pytest
+from pymongo import MongoClient
+
+__author__ = "Yiwen Sun and Ziyou Zhang"
+__status__ = "Prototype"
+   
+def test_connection():
+    pass
+
+def test_data_vlidity():
+    with MongoClient('mongodb://admin:sentrade@45.76.133.175:27017') as client:
+        db = client.sentrade_db
+        stock_db = db.stock_price
+        all_stock = stock_db.find()
+        for stock in all_stock:
+            assert stock['open'] >= 0
+            assert stock['high'] >= 0
+            assert stock['low'] >= 0 
+            assert stock['close'] >= 0
+            assert stock['volume'] >= 0
+
+if __name__ == "__main__":
+    pass
