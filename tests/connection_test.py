5258b9b087f443901ee62bd927e5701fa153cd0e
Yiwen Sun <yiwen.sun2017@yahoo.com>
2020-02-24 19:46:54 +0000

    function name update

--- tests/connection_test.py
+++ tests/connection_test.py
@@ -8,10 +8,10 @@ __author__ = "Yiwen Sun and Ziyou Zhang"
 __status__ = "Prototype"
       
 def test_connection():
-    with MongoClient('mongodb://admin:sentrade@45.76.133.175:27016') as client:
-        db=client.admin
-        serverStatusResult=db.command("serverStatus")
-        print(serverStatusResult)
+    # with MongoClient('mongodb://admin:sentrade@45.76.133.175:27016') as client:
+    #     db=client.admin
+    #     serverStatusResult=db.command("serverStatus")
+    #     print(serverStatusResult)
     pass
 
 if __name__ == "__main__":
