3eadf0f728d4687a5e6f5604bf5f4b7ccdca2193
Yiwen Sun <yiwen.sun2017@yahoo.com>
2020-02-24 21:02:31 +0000

    multi-connection test update

--- tests/connection_test.py
+++ tests/connection_test.py
@@ -2,17 +2,38 @@
 # -*- coding: utf-8 -*-
 
 import pytest
+from pprint import pprint
 from pymongo import MongoClient
+from multiprocessing import Process
 
 __author__ = "Yiwen Sun and Ziyou Zhang"
 __status__ = "Prototype"
       
 def test_connection():
-    # with MongoClient('mongodb://admin:sentrade@45.76.133.175:27016') as client:
-    #     db=client.admin
-    #     serverStatusResult=db.command("serverStatus")
-    #     print(serverStatusResult)
-    pass
+    with MongoClient('mongodb://admin:sentrade@45.76.133.175:27017') as client:
+        db=client.admin
+        serverStatusResult=db.command("serverStatus")
+        assert serverStatusResult["connections"]["active"] > 0
 
+def get_connection_count():
+    with MongoClient('mongodb://admin:sentrade@45.76.133.175:27017') as client:
+        db=client.admin
+        serverStatusResult=db.command("serverStatus")
+        return serverStatusResult["connections"]["active"]
+
+def test_multi_connections():
+    procs = []
+    for i in range(1000):
+        proc = Process(target=get_connection_count)
+        procs.append(proc)
+        proc.start()
+
+    for proc in procs:
+        proc.join()
+    
+    #will only reach here if all tests passed
+    assert True
+
+    
 if __name__ == "__main__":
-    pass
+    test_multi_connections()
