d24811502fb1e4c4f8cfa99aa9ac1df801a5ab1b
Yiwen Sun <yiwen.sun2017@yahoo.com>
2020-04-03 18:48:20 +0100

    updated stock price database code

--- stock_price_scraping/stock_price.py
+++ stock_price_scraping/stock_price.py
@@ -7,6 +7,7 @@ __status__ = "Production"
 import yfinance as yf
 import json
 from pymongo import MongoClient
+
 # import pandas as pd
 
 # def is_business_day(date):
@@ -19,6 +20,7 @@ def history_stock_price(stock_name):
     :param stock_name:stock ticker
     :return: json file including 'date', 'open','high','low','close','volume'
     """
+    
     stock = yf.Ticker(stock_name)  
     hist = stock.history(start="2018-11-01")  
     
@@ -30,7 +32,6 @@ def history_stock_price(stock_name):
     for hist_key, hist_value in hist.items():
         temp[hist_key] = list(hist_value)
     results = {}
-    print (hist_value)
     stock_count = len(list(hist_value))
     results = [{'date': "", 'company_name':"", 'open': 0, 'high':0, 'low': 0, 'close':0, 'volume':0, 'change': 0, 'rise': 0} for x in range(stock_count)]
     for i in range(stock_count):
@@ -45,12 +46,69 @@ def history_stock_price(stock_name):
     for i in range(1, stock_count):
         results[i]['change'] = results[i]['close'] - results[i-1]['close']
         results[i]['rise'] = 1 if results[i]['change'] > 0 else 0
+    
+    print("current ticker:", stock_name)
 
     return results
 
+def stock_price_database(company_name, client_address):
+    """
+	get historical stock price and put in the stock database.
+
+    :param company_name: name of the company
+    :param client_address: the database address
+    """
+
+    company_to_ticker = {
+        "amazon"    : "AMZN",
+        "apple"     : "AAPL", 
+        "facebook"  : "FB",
+        "google"    : "GOOG",
+        "microsoft" : "MSFT",
+        "netflix"   : "NFLX",
+        "tesla"     : "TSLA",
+        "uber"      : "UBER"
+    }
+
+    stock_name = company_to_ticker[company_name]
+
+    stock = yf.Ticker(stock_name)
+    hist = stock.history(start="2018-11-01")
+    
+    # create the temp list dictionary
+    temp = {}
+    temp['Date'] = []
+    for date in hist.index:
+        temp['Date'].append(str(date)[:10])
+    for hist_key, hist_value in hist.items():
+        temp[hist_key] = list(hist_value)
+    results = {}
+
+    stock_count = len(list(hist_value))
+    results = [{'date': "", 'company_name':"", 'open': 0, 'high':0, 'low': 0, 'close':0, 'volume':0, 'change': 0, 'rise': 0} for x in range(stock_count)]
+    for i in range(stock_count):
+        results[i]['company'] = company_name
+        results[i]['open'] = temp['Open'][i]
+        results[i]['date'] = temp['Date'][i]
+        results[i]['high'] = temp['High'][i]
+        results[i]['low'] = temp['Low'][i]
+        results[i]['close'] = temp['Close'][i]
+        results[i]['volume'] = temp['Volume'][i]
+    
+    for i in range(1, stock_count):
+        results[i]['change'] = results[i]['close'] - results[i-1]['close']
+        results[i]['rise'] = 1 if results[i]['change'] > 0 else 0
+
+    client = MongoClient(client_address)
+    db = client.stock_data
+    stock_db = db[company_name]
+    stock_db.drop()
+    stock_db.insert_many(results)
+    client.close()
+
+    print("current company:", company_name)
+
 if __name__ == "__main__":
-    from pymongo import MongoClient
-    import json
 
     client = MongoClient('mongodb://admin:sentrade@45.76.133.175:27017')
     stock_list = ['AAPL', 'TSLA', 'AMZN', 'FB', 'GOOG', 'MSFT', 'NFLX', 'UBER']
@@ -64,3 +122,8 @@ if __name__ == "__main__":
         stock_db.insert_many(stock_output)    
 
     client.close()
+
+    # client_address = 'mongodb://admin:sentrade@45.76.133.175:27017'
+    # companies = ["apple", "amazon", "facebook", "google", "microsoft", "netflix", "tesla", "uber"]
+    # for company in companies:
+    #     stock_price_database(company, client_address)
