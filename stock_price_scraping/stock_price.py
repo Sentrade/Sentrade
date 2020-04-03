138de64da2b5f3333dd16fa746ad47360e8fd9bc
Yiwen Sun <yiwen.sun2017@yahoo.com>
2020-04-03 13:33:22 +0100

    added labels to stock price

--- stock_price_scraping/stock_price.py
+++ stock_price_scraping/stock_price.py
@@ -31,8 +31,9 @@ def history_stock_price(stock_name):
         temp[hist_key] = list(hist_value)
     results = {}
     print (hist_value)
-    results = [{'date': "", 'company_name':"", 'open': 0, 'high':0, 'low': 0, 'close':0, 'volume':0} for x in range(len(list(hist_value)))]
-    for i in range(len(list(hist_value))):
+    stock_count = len(list(hist_value))
+    results = [{'date': "", 'company_name':"", 'open': 0, 'high':0, 'low': 0, 'close':0, 'volume':0, 'change': 0, 'rise': 0} for x in range(stock_count)]
+    for i in range(stock_count):
         results[i]['company_name'] = stock_name
         results[i]['open'] = temp['Open'][i]
         results[i]['date'] = temp['Date'][i]
@@ -40,6 +41,10 @@ def history_stock_price(stock_name):
         results[i]['low'] = temp['Low'][i]
         results[i]['close'] = temp['Close'][i]
         results[i]['volume'] = temp['Volume'][i]
+    
+    for i in range(1, stock_count):
+        results[i]['change'] = results[i]['close'] - results[i-1]['close']
+        results[i]['rise'] = 1 if results[i]['change'] > 0 else 0
 
     return results
 
@@ -47,19 +52,14 @@ if __name__ == "__main__":
     from pymongo import MongoClient
     import json
 
-    # Setup the connection.
     client = MongoClient('mongodb://admin:sentrade@45.76.133.175:27017')
+    stock_list = ['AAPL', 'TSLA', 'AMZN', 'FB', 'GOOG', 'MSFT', 'NFLX', 'UBER']
 
-    # Use the database sentrade_db.
     db = client.sentrade_db
-
-    # Select the table to use.
     stock_db = db.stock_price
     stock_db.drop()
-    stock_list = ['AAPL', 'TSLA', 'AMZN','FB', 'GOOG', 'MSFT', 'NFLX', 'UBER']
 
     for stocks in stock_list: 
-        # if date today exist
         stock_output = history_stock_price(stock_name=stocks)
         stock_db.insert_many(stock_output)    
 
