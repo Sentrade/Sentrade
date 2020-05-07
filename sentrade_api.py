e3ef7a568e529b64d0b405318f0985b021563fda
Ziyou Zhang <ziyou.zhang@outlook.com>
2020-05-08 04:57:04 +0800

    finished sentrade api development

---
+++ sentrade_api.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+from pymongo import MongoClient
+from bson.json_util import dumps
+
+__author__ = "Ziyou Zhang"
+__status__ = "Production"
+
+app = Flask(__name__)
+app.config["DEBUG"] = False
+
+client_address = "mongodb://admin:sentrade@45.76.133.175:27017"
+
+@app.route('/api/data/apple', methods=['GET'])
+def apple_data():
+    client = MongoClient(client_address)
+    db = client.sentiment_data["apple"]
+    sentiment_data = db.find()
+    data = []
+    for sentiment in sentiment_data:
+        data.append(sentiment)
+    client.close()
+    return dumps(data)
+
+@app.route('/api/data/amazon', methods=['GET'])
+def amazon_data():
+    client = MongoClient(client_address)
+    db = client.sentiment_data["amazon"]
+    sentiment_data = db.find()
+    data = []
+    for sentiment in sentiment_data:
+        data.append(sentiment)
+    client.close()
+    return dumps(data)
+
+@app.route('/api/data/facebook', methods=['GET'])
+def facebook_data():
+    client = MongoClient(client_address)
+    db = client.sentiment_data["facebook"]
+    sentiment_data = db.find()
+    data = []
+    for sentiment in sentiment_data:
+        data.append(sentiment)
+    client.close()
+    return dumps(data)
+
+@app.route('/api/data/google', methods=['GET'])
+def google_data():
+    client = MongoClient(client_address)
+    db = client.sentiment_data["google"]
+    sentiment_data = db.find()
+    data = []
+    for sentiment in sentiment_data:
+        data.append(sentiment)
+    client.close()
+    return dumps(data)
+
+@app.route('/api/data/microsoft', methods=['GET'])
+def microsoft_data():
+    client = MongoClient(client_address)
+    db = client.sentiment_data["microsoft"]
+    sentiment_data = db.find()
+    data = []
+    for sentiment in sentiment_data:
+        data.append(sentiment)
+    client.close()
+    return dumps(data)
+
+@app.route('/api/data/netflix', methods=['GET'])
+def netflix_data():
+    client = MongoClient(client_address)
+    db = client.sentiment_data["netflix"]
+    sentiment_data = db.find()
+    data = []
+    for sentiment in sentiment_data:
+        data.append(sentiment)
+    client.close()
+    return dumps(data)
+
+@app.route('/api/data/tesla', methods=['GET'])
+def tesla_data():
+    client = MongoClient(client_address)
+    db = client.sentiment_data["tesla"]
+    sentiment_data = db.find()
+    data = []
+    for sentiment in sentiment_data:
+        data.append(sentiment)
+    client.close()
+    return dumps(data)
+
+@app.route('/api/data/uber', methods=['GET'])
+def uber_data():
+    client = MongoClient(client_address)
+    db = client.sentiment_data["uber"]
+    sentiment_data = db.find()
+    data = []
+    for sentiment in sentiment_data:
+        data.append(sentiment)
+    client.close()
+    return dumps(data)
+
+app.run(host='0.0.0.0', port=8080)
