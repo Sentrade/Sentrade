766a109646e6241fbebc4a3ef353a3e0c1cf557d
yinshaomiao <yinshaomiao@gmail.com>
2020-05-07 12:57:29 +0100

    what the fuck

--- data_analysis/clf_models_3.py
+++ data_analysis/clf_models_3.py
@@ -15,7 +15,7 @@ from sklearn.metrics import accuracy_score, confusion_matrix, plot_confusion_mat
 __author__ = "Fengming Liu, Longzhen Li, Shaomiao Yin"
 __status__ = "Development"
 
-    
+
 def classifier_run(clf, name, x_train, x_test, y_train, y_test):
     clf.fit(x_train,y_train)
     y_pred = clf.predict(x_test)
@@ -34,8 +34,8 @@ def write_result_csv(csv_file, alg_name, accuracy, cm, n_cat=5):
 total_df = pd.read_csv("./total_data.csv")
 total_df.dropna(inplace=True)
 response = "up_cat"
-feat_date_company = ["relative_day", "company_amazon", "company_apple", 
-                    "company_facebook", "company_google", "company_microsoft", 
+feat_date_company = ["relative_day", "company_amazon", "company_apple",
+                    "company_facebook", "company_google", "company_microsoft",
                     "company_netflix", "company_tesla"]
 distinct_features_list = [["7_day_sentiment_score"],
                           ["7_day_bert_sentiment_score"],
@@ -61,13 +61,13 @@ for distinct_features in distinct_features_list:
     result_csv.write('\n')
     result_csv.write("response:," + response + '\n')
     result_csv.write(" ,\n")
-    
+
     # prepare the data
     x_train, x_test, y_train, y_test = train_test_split(total_df[features].to_numpy(),
                                                         total_df[response],
                                                         test_size=0.3,
                                                         shuffle=True,
-                                                        random_state=500)    
+                                                        random_state=500)
     # train the model
     for alg_name, clf in alg_dict.items():
         [accuracy, clf] = classifier_run(clf, alg_name, x_train, x_test, y_train, y_test)
@@ -76,13 +76,13 @@ for distinct_features in distinct_features_list:
                                      cmap=plt.cm.Blues)
         disp.ax_.set_title(alg_name)
         plt.savefig("./results/{0}.png".format(alg_name))
-        
+
         print("distinct features:", distinct_features)
         print("algorithm:", alg_name)
         print("accuracy:", accuracy)
         print(disp.confusion_matrix)
         print()
-        
+
         joblib.dump(clf, "./models/clf_{0}_model.joblib".format(alg_name))
         write_result_csv(result_csv, alg_name, accuracy, disp.confusion_matrix)
     result_csv.write('\n')
