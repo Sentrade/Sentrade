c42eeb8b78a6d15733c6a79c8934cc0c5387bb1e
Ziyou Zhang <ziyou.zhang@outlook.com>
2020-05-12 01:25:13 +0800

    updated about us

--- sentrade.py
+++ sentrade.py
@@ -39,9 +39,8 @@ This is a temporary website built by 6 MSc students from Imperial College London
 SenTrade makes no express or implied warranty that the website will be updated timely: please do not use it for trading purposes. SenTrade will not be liable for any damages or losses caused by the use of information provided.
 
 If you are interested in the raw sentiment data to do your own analysis, call REST API via
-www.api-sentrade.doc.ic.ac.uk/data/<company_name>
-where company name can be chosen from:
-apple, amazon, facebook, google, microsoft, netflix, tesla, uber.
+http://api-sentrade.doc.ic.ac.uk/data/<company_name>
+where company name can be chosen from: apple, amazon, facebook, google, microsoft, netflix, tesla, uber.
 """
 
 easter_egg_message = """
